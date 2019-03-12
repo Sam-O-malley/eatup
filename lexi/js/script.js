835a5cf2dad359c61540f5f2c95db28e8a431d2d
lexi-jayy <42328763+lexi-jayy@users.noreply.github.com>
2019-03-12 17:16:49 +1300

    updated html and js

--- lexi/js/script.js
+++ lexi/js/script.js
@@ -1,1 +1,18 @@
-$ajax
+function getData() {
+
+    $.ajax({
+           // url: 'https://developers.zomato.com/api/v2.1/reviews?res_id=7100538',
+             url: 'https://developers.zomato.com/api/v2.1/cuisines?city_id=71',
+    
+           dataType: 'json',
+           type: 'GET',
+           async: true,
+           beforeSend: function(xhr){xhr.setRequestHeader('user-key',
+           '0d4d795b7b6b0e26ad6f107918004ec4');
+       },  // This inserts the api key into the HTTP header
+           success: function(response){ 
+               console.log(response) 
+            }
+           // document.getElementById("mainContainer").innerHTML = "";
+       });
+    }    
