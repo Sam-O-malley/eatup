c3a471900b7112d5f276effa41ad0e36ad225df6
Lexi-Jayy Critchley <845869864@bun2-204-03.local>
2019-03-13 16:41:22 +1300

    showing in console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("certain death after formative");
+function getData() {
+
+    $.ajax({
+           // url: 'https://developers.zomato.com/api/v2.1/reviews?res_id=7100538',
+             url: 'https://developers.zomato.com/api/v2.1/location_details?entity_id=36932&entity_type=group',
+    
+           dataType: 'json',
+           type: 'GET',
+           async: true,
+           beforeSend: function(xhr){xhr.setRequestHeader('user-key',
+           'a952103b6e48c2a40500499f78ff2c5b');
+       },  // This inserts the api key into the HTTP header
+           success: function(response){ 
+               console.log(response) 
+            }
+           // document.getElementById("mainContainer").innerHTML = "";
+       });
+    }  
+    getData(); 
