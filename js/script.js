f917469326c4a5d8df340d13ad321b9db7b3a0b5
Lexi-Jayy Critchley <845869864@bun2-206-18.local>
2019-03-18 15:26:58 +1300

    lexi edited

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function getData() {
 
     $.ajax({
            // url: 'https://developers.zomato.com/api/v2.1/reviews?res_id=7100538',
-             url: 'https://developers.zomato.com/api/v2.1/location_details?entity_id=36932&entity_type=group',
+             url: 'https://developers.zomato.com/api/v2.1/location_details?city_id=71&entity_id=36932&entity_type=group',
     
            dataType: 'json',
            type: 'GET',
@@ -12,11 +12,12 @@ function getData() {
        },  // This inserts the api key into the HTTP header
 
            success: function(response){
-               console.log(response);
+              //  console.log(response);
                var i =0
                for (i = 0; response.best_rated_restaurant.length; i++){
-               console.log(response.best_rated_restaurant[0].restaurant.name, response.best_rated_restaurant[0].restaurant.location.address, response.best_rated_restaurant[0].restaurant.average_cost_for_two,  );
-               }
+              //  console.log(response.best_rated_restaurant[0].restaurant.name, response.best_rated_restaurant[0].restaurant.location.address, response.best_rated_restaurant[0].restaurant.average_cost_for_two,  );
+               console.log(response.best_rated_restaurant[i].restaurant.average_cost_for_two); 
+              }
             }
        });
     }  
