692e93505d7af0a528dabc15998a836342c0c210
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-03-20 23:44:36 +0000

    merging sam's branch

--- js/script.js
+++ js/script.js
@@ -13,24 +13,57 @@ url: 'https://developers.zomato.com/api/v2.1/location_details?entity_id=71&entit
    dataType: 'json',
    type: 'GET',
    async: true,
-   beforeSend: function(xhr){xhr.setRequestHeader('user-key','0d4d795b7b6b0e26ad6f107918004ec4');
+   beforeSend: function(xhr){ xhr.setRequestHeader('user-key','0d4d795b7b6b0e26ad6f107918004ec4');
       },// This inserts the api key into the HTTP header
         success: function(response) 
       { 
       console.log(response);
         
+        var zomato_api = document.getElementById('zomato_data');
+
       for(var i = 0; i < response.best_rated_restaurant.length; i++){
           
-      
-        console.log(response.best_rated_restaurant[i].restaurant.name,response.best_rated_restaurant[i].restaurant.location.address,
-          response.best_rated_restaurant[i].restaurant.user_rating.aggregate_rating); 
-         document.getElementById('dataBox').innerHTML += "<div class='rest_name'>" + response.best_rated_restaurant[i].restaurant.name + "</div>" + response.best_rated_restaurant[i].restaurant.location.address + response.best_rated_restaurant[i].restaurant.user_rating.aggregate_rating +"<br/>";
+     var api_container = document.createElement('div');
+
+     console.log(response.best_rated_restaurant[0].restaurant.offers[i].photos_url);
+
+        var zomato_img = document.createElement("img");
+
+        zomato_img.src = (response.best_rated_restaurant[i].restaurant.photos_url);
+
+        var src = document.getElementById('zomato_img-display');
+
+        // src.appendChild(zomato_img);
+
+     api_container.className += 'card-body databox';
+
+     api_container.innerHTML += ( '<img class=img_container id=zomato_img-display src="response.best_rated_restaurant[i].restaurant.offers.photos_url">' );
+
+     api_container.innerHTML += ( '<h3 class=card-title id="restaurant_title">' + response.best_rated_restaurant[i].restaurant.name);
+
+      api_container.innerHTML += ('<span> Address </span><br><p class=card-title  id="restaurant_address">'  + response.best_rated_restaurant[i].restaurant.location.address);
+
+      api_container.innerHTML += (' <span> Rating </span><p class=card-title id="restaurant_rating" >' + response.best_rated_restaurant[i].restaurant.user_rating.aggregate_rating);
+
+      console.log(api_container.innerHTML += ('<h4 class=card-title>' + response.best_rated_restaurant[i].restaurant.cuisines));
 
 
+
+          console.log(zomato_api);  
+            zomato_api.appendChild(api_container);
          }
+
+
+
+
+
+
+
 
       } 
     
 
     });
+
     }
+
